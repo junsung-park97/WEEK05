5d164ad1be69b341d96b8744dc57a1b38436041d
junSung <not971006@gmail.com>
2025-08-13 10:18:23 +0900

    링크드리스트4, 5번문제

--- Data-Structures/Linked_List/Q5_A_LL.c
+++ Data-Structures/Linked_List/Q5_A_LL.c
@@ -11,42 +11,44 @@ Purpose: Implementing the required functions for Question 5 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
-typedef struct _listnode{
+typedef struct _listnode
+{
 	int item;
 	struct _listnode *next;
-} ListNode;			// You should not change the definition of ListNode
+} ListNode; // You should not change the definition of ListNode
 
-typedef struct _linkedlist{
+typedef struct _linkedlist
+{
 	int size;
 	ListNode *head;
-} LinkedList;			// You should not change the definition of LinkedList
-
+} LinkedList; // You should not change the definition of LinkedList
 
 ///////////////////////// function prototypes ////////////////////////////////////
 
 // You should not change the prototype of this function
-void frontBackSplitLinkedList(LinkedList* ll, LinkedList *resultFrontList, LinkedList *resultBackList);
+void frontBackSplitLinkedList(LinkedList *ll, LinkedList *resultFrontList, LinkedList *resultBackList);
 
 void printList(LinkedList *ll);
 void removeAllItems(LinkedList *l);
-ListNode * findNode(LinkedList *ll, int index);
+ListNode *findNode(LinkedList *ll, int index);
 int insertNode(LinkedList *ll, int index, int value);
 int removeNode(LinkedList *ll, int index);
-
 
 ///////////////////////////// main() /////////////////////////////////////////////
 
 int main()
 {
 	int c, i;
+	
+	c = 1;
 	LinkedList ll;
 	LinkedList resultFrontList, resultBackList;
 
-	//Initialize the linked list as an empty linked list
+	// Initialize the linked list as an empty linked list
 	ll.head = NULL;
 	ll.size = 0;
 
-	//Initialize the front linked list as an empty linked list
+	// Initialize the front linked list as an empty linked list
 	resultFrontList.head = NULL;
 	resultFrontList.size = 0;
 
@@ -60,7 +62,7 @@ int main()
 
 	while (c != 0)
 	{
-	    printf("Please input your choice(1/2/0): ");
+		printf("Please input your choice(1/2/0): ");
 		scanf("%d", &c);
 
 		switch (c)
@@ -102,12 +104,43 @@ int main()
 
 void frontBackSplitLinkedList(LinkedList *ll, LinkedList *resultFrontList, LinkedList *resultBackList)
 {
-	/* add your code here */
+	if (ll == NULL || ll->head == NULL)
+	{
+		return;
+	}
+    // 2. 크기가 1인 경우 front에 배치
+    if (ll->size == 1) {
+        insertNode(resultFrontList, 0, ll->head->item);
+        return;
+    }
+
+	ListNode *first = ll->head;
+	ListNode *second = ll->head;
+
+	// 노드가 홀수이면 second의 next가 NULL이지만, 짝수이면 second 자체가 NULL임 그래서 ||연산자 사용해서 해야됨
+	while (second != NULL && second->next != NULL)
+	{
+		if(second != NULL && second->next != NULL)1
+		{
+			second = second->next->next;
+			first = first->next;
+		}
+	}
+	for(ListNode* ptr = ll->head; ptr == first; ptr=ptr->next)
+	{
+		insertNode(resultFrontList, resultFrontList->size, first->item);
+	}
+	for(first; first->next == NULL; first=first->next)
+	{
+		insertNode(resultBackList, resultBackList->size, first->item);
+	}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-void printList(LinkedList *ll){
+void printList(LinkedList *ll)
+{
 
 	ListNode *cur;
 	if (ll == NULL)
@@ -123,13 +156,13 @@ void printList(LinkedList *ll){
 	printf("\n");
 }
 
-
 void removeAllItems(LinkedList *ll)
 {
 	ListNode *cur = ll->head;
 	ListNode *tmp;
 
-	while (cur != NULL){
+	while (cur != NULL)
+	{
 		tmp = cur->next;
 		free(cur);
 		cur = tmp;
@@ -138,8 +171,8 @@ void removeAllItems(LinkedList *ll)
 	ll->size = 0;
 }
 
-
-ListNode * findNode(LinkedList *ll, int index){
+ListNode *findNode(LinkedList *ll, int index)
+{
 
 	ListNode *temp;
 
@@ -151,7 +184,8 @@ ListNode * findNode(LinkedList *ll, int index){
 	if (temp == NULL || index < 0)
 		return NULL;
 
-	while (index > 0){
+	while (index > 0)
+	{
 		temp = temp->next;
 		if (temp == NULL)
 			return NULL;
@@ -161,7 +195,8 @@ ListNode * findNode(LinkedList *ll, int index){
 	return temp;
 }
 
-int insertNode(LinkedList *ll, int index, int value){
+int insertNode(LinkedList *ll, int index, int value)
+{
 
 	ListNode *pre, *cur;
 
@@ -169,7 +204,8 @@ int insertNode(LinkedList *ll, int index, int value){
 		return -1;
 
 	// If empty list or inserting first node, need to update head pointer
-	if (ll->head == NULL || index == 0){
+	if (ll->head == NULL || index == 0)
+	{
 		cur = ll->head;
 		ll->head = malloc(sizeof(ListNode));
 		ll->head->item = value;
@@ -180,7 +216,8 @@ int insertNode(LinkedList *ll, int index, int value){
 
 	// Find the nodes before and at the target position
 	// Create a new node and reconnect the links
-	if ((pre = findNode(ll, index - 1)) != NULL){
+	if ((pre = findNode(ll, index - 1)) != NULL)
+	{
 		cur = pre->next;
 		pre->next = malloc(sizeof(ListNode));
 		pre->next->item = value;
@@ -192,8 +229,8 @@ int insertNode(LinkedList *ll, int index, int value){
 	return -1;
 }
 
-
-int removeNode(LinkedList *ll, int index){
+int removeNode(LinkedList *ll, int index)
+{
 
 	ListNode *pre, *cur;
 
@@ -202,7 +239,8 @@ int removeNode(LinkedList *ll, int index){
 		return -1;
 
 	// If removing first node, need to update head pointer
-	if (index == 0){
+	if (index == 0)
+	{
 		cur = ll->head->next;
 		free(ll->head);
 		ll->head = cur;
@@ -213,7 +251,8 @@ int removeNode(LinkedList *ll, int index){
 
 	// Find the nodes before and after the target position
 	// Free the target node and reconnect the links
-	if ((pre = findNode(ll, index - 1)) != NULL){
+	if ((pre = findNode(ll, index - 1)) != NULL)
+	{
 
 		if (pre->next == NULL)
 			return -1;
--- Data-Structures/test.c
+++ Data-Structures/test.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
 // int main (void){
 //     int* pList = NULL, i = 0;
@@ -23,7 +23,7 @@
 
 //     // 배열이므로 stack영역에 저장됨
 //     char szBuffer[] = {"hello"};
-//     // 리터럴 값의 주소를 가르키는 포인터변수를 선언함으로 정적영역에 리터럴값이 저장되고 stack영역에 포인터변수의 메모리가 할당된다. 
+//     // 리터럴 값의 주소를 가르키는 포인터변수를 선언함으로 정적영역에 리터럴값이 저장되고 stack영역에 포인터변수의 메모리가 할당된다.
 //     // 즉, 밑의 코드를 선언함으로 두번의 메모리할당이 진행됨.
 //     // 포인터 변수의 경우 8byte를 할당받음.
 //     char* pszBuffer = "hello";
@@ -46,27 +46,60 @@
 
 // }
 
-typedef struct USERDATA
+// typedef struct USERDATA
+// {
+//     int nAge;
+//     char szName[32];
+//     char szPhon [32];
+
+// } USERDATA;
+
+// int main(void)
+// {
+//     USERDATA * pUser = NULL;
+
+//     pUser = (USERDATA*)malloc(sizeof(USERDATA));
+
+//     pUser -> nAge = 10;
+//     strcpy_s(pUser -> szName, sizeof(pUser -> szName),"Hoon");
+//     strcpy_s(pUser -> szPhon, sizeof(pUser -> szPhon), "9876");
+
+//     printf("%d살\t%s\t%s\n", pUser -> nAge, pUser -> szName, pUser -> szPhon);
+
+//     free(pUser);
+
+//     return 0;
+// }
+
+// // 값복사
+// int main (void) {
+//     char szData[12] = {"HelloWorld"};
+//     char* pszData = NULL;
+
+//     pszData = (char*)malloc(sizeof(char) * 12);
+//     for (int i = 0; i < 12; i++)
+//     {
+//         pszData[i] = szData[i];
+//     }
+//     puts(pszData);
+//     free(pszData);
+// }
+
+// 예제
+#include <stdio.h>
+#include <stdlib.h>
+
+int maon(void)
 {
-    int nAge;
-    char szName[32];
-    char szPhon [32];
+    char szBuffer[12] = {"HelloWorld"};
+    char *pszData = NULL;
+    pszData = (char *)malloc(sizeof(char) * 12);
 
-} USERDATA;
-
-int main(void)
-{
-    USERDATA * pUser = NULL;
-
-    pUser = (USERDATA*)malloc(sizeof(USERDATA));
-
-    pUser -> nAge = 10;
-    strcpy_s(pUser -> szName, sizeof(pUser -> szName),"Hoon");
-    strcpy_s(pUser -> szPhon, sizeof(pUser -> szPhon), "9876");
-
-    printf("%d살\t%s\t%s\n", pUser -> nAge, pUser -> szName, pUser -> szPhon);
-
-    free(pUser);
-    
+    // pszData = szBuffer;
+    for (int i = 0; i < 12; ++i)
+    {
+        pszData[i] = szBuffer[i];
+    }
+    puts(pszData);
     return 0;
 }
